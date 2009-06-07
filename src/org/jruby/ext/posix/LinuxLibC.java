d85ffcdfeb94a9d4106e72170cf1dc1fa22b2f91
Wayne Meissner <wmeissner@gmail.com>
2009-06-07 12:10:49 +1000

    Mark up some native functions with @Out @In @Transient annotations to tweak performance

--- src/org/jruby/ext/posix/LinuxLibC.java
+++ src/org/jruby/ext/posix/LinuxLibC.java
@@ -1,10 +1,20 @@
 package org.jruby.ext.posix;
 
+import com.kenai.jaffl.annotations.In;
+import com.kenai.jaffl.annotations.NulTerminate;
+import com.kenai.jaffl.annotations.Out;
+import com.kenai.jaffl.annotations.Transient;
+import java.nio.ByteBuffer;
+
 public interface LinuxLibC extends LibC {
-    public int __fxstat(int version, int fd, FileStat stat);
-    public int __lxstat(int version, String path, FileStat stat);
-    public int __xstat(int version, String path, FileStat stat);
-    public int __fxstat64(int version, int fd, FileStat stat);
-    public int __lxstat64(int version, String path, FileStat stat);
-    public int __xstat64(int version, String path, FileStat stat);
+    public int __fxstat(int version, int fd, @Out @Transient FileStat stat);
+    public int __lxstat(int version, CharSequence path, @Out @Transient FileStat stat);
+    public int __lxstat(int version, @NulTerminate @In ByteBuffer path, @Out @Transient FileStat stat);
+    public int __xstat(int version, CharSequence path, @Out @Transient FileStat stat);
+    public int __xstat(int version, @NulTerminate @In ByteBuffer path, @Out @Transient FileStat stat);
+    public int __fxstat64(int version, int fd, @Out @Transient FileStat stat);
+    public int __lxstat64(int version, CharSequence path, @Out @Transient FileStat stat);
+    public int __lxstat64(int version, @NulTerminate @In ByteBuffer path, @Out @Transient FileStat stat);
+    public int __xstat64(int version, CharSequence path, @Out @Transient FileStat stat);
+    public int __xstat64(int version, @NulTerminate @In ByteBuffer path, @Out @Transient FileStat stat);
 }
