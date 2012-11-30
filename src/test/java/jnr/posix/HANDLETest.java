4fda66067f93de67a677430086adfd2ee880afc9
Thomas E. Enebo <tom.enebo@gmail.com>
2012-11-30 14:07:15 -0600

    handle type for std value can vary depending on how a process is supplied them.  Make test a little more general

--- src/test/java/jnr/posix/HANDLETest.java
+++ src/test/java/jnr/posix/HANDLETest.java
@@ -21,12 +21,21 @@ public class HANDLETest {
     private static WindowsLibC kernel32() {
         return (WindowsLibC) posix.libc();
     }
+    
+    // This really is matching pipe for the sake of these unit tests but in general the 
+    // in/out/error handle will vary depending on how a CLI supplies it: 
+    // http://stackoverflow.com/questions/9021916/how-do-i-check-if-my-delphi-console-app-is-redirected-to-a-file-or-pipe
+    private boolean isValidHandleType(HANDLE handle) {
+        int type = kernel32().GetFileType(handle);
+        
+        return type == WindowsLibC.FILE_TYPE_DISK || type == WindowsLibC.FILE_TYPE_PIPE;
+    }
 
     @Test
     public void stdinHandle() {
         if (Platform.IS_WINDOWS) {
             assertTrue(kernel32().GetStdHandle(WindowsLibC.STD_INPUT_HANDLE).isValid());
-            assertEquals(WindowsLibC.FILE_TYPE_CHAR, kernel32().GetFileType(kernel32().GetStdHandle(WindowsLibC.STD_INPUT_HANDLE)));
+            assertTrue(isValidHandleType(kernel32().GetStdHandle(WindowsLibC.STD_INPUT_HANDLE)));
         }
     }
 
@@ -34,7 +43,7 @@ public class HANDLETest {
     public void stdoutHandle() {
         if (Platform.IS_WINDOWS) {
             assertTrue(kernel32().GetStdHandle(WindowsLibC.STD_OUTPUT_HANDLE).isValid());
-            assertEquals(WindowsLibC.FILE_TYPE_CHAR, kernel32().GetFileType(kernel32().GetStdHandle(WindowsLibC.STD_OUTPUT_HANDLE)));
+            assertTrue(isValidHandleType(kernel32().GetStdHandle(WindowsLibC.STD_OUTPUT_HANDLE)));
         }
     }
 
@@ -42,7 +51,7 @@ public class HANDLETest {
     public void stderrHandle() {
         if (Platform.IS_WINDOWS) {
             assertTrue(kernel32().GetStdHandle(WindowsLibC.STD_ERROR_HANDLE).isValid());
-            assertEquals(WindowsLibC.FILE_TYPE_CHAR, kernel32().GetFileType(kernel32().GetStdHandle(WindowsLibC.STD_ERROR_HANDLE)));
+            assertTrue(isValidHandleType(kernel32().GetStdHandle(WindowsLibC.STD_ERROR_HANDLE)));
         }
     }
 }
