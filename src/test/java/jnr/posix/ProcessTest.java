38e2ab95cc9e2a727747e2429ce31893b2038aa2
Charles Oliver Nutter <headius@headius.com>
2014-05-31 14:24:44 -0500

    Add a test for posix_spawn. May not work on all platforms.

--- src/test/java/jnr/posix/ProcessTest.java
+++ src/test/java/jnr/posix/ProcessTest.java
@@ -2,8 +2,15 @@ package jnr.posix;
 
 import com.kenai.jffi.Platform;
 import jnr.constants.platform.Sysconf;
+import jnr.posix.util.FieldAccess;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.channels.Pipe;
+import java.util.Arrays;
+import java.util.Collections;
+
 import static org.junit.Assert.*;
 
 public class ProcessTest {
@@ -21,5 +28,23 @@ public class ProcessTest {
         if (Platform.getPlatform().getOS() == Platform.OS.DARWIN) {
             assertEquals("incorrect HZ value", 100L, hz);
         }
+    }
+
+    @Test
+    public void posix_spawn() throws Throwable {
+        long pid = posix.posix_spawnp("ls", null, null, Arrays.asList("."), Arrays.asList("FOO=BAR"));
+
+        assertTrue(pid > 0);
+
+        Pipe pipe = Pipe.open();
+        pipe.sink().configureBlocking(false);
+        int fd1, fd2;
+
+        fd1 = (Integer)FieldAccess.getProtectedFieldValue(Class.forName("sun.nio.ch.SourceChannelImpl"), "fdVal", pipe.source());
+        fd2 = (Integer)FieldAccess.getProtectedFieldValue(Class.forName("sun.nio.ch.SinkChannelImpl"), "fdVal", pipe.sink());
+        pid = posix.posix_spawnp("yes", Arrays.asList(SpawnFileAction.dup(fd2, 1), SpawnFileAction.close(fd1)), null, Collections.EMPTY_LIST, Collections.EMPTY_LIST);
+
+        posix.close(fd1);
+        posix.waitpid(pid, null, 0);
     }
 }
