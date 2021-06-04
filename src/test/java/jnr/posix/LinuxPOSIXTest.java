21e451e92f65e5fed0d59df68af1d635359d83ee
Charles Oliver Nutter <headius@headius.com>
2021-06-04 13:44:54 -0500

    Revert "Expand condition to skip PPC until jnr-constants handles arch"
    This reverts commit e8502f4c97d30d8c71c4eb76db04855b97aab08b.

--- src/test/java/jnr/posix/LinuxPOSIXTest.java
+++ src/test/java/jnr/posix/LinuxPOSIXTest.java
@@ -20,30 +20,16 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.RunnableFuture;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.Lock;
 
 import static jnr.posix.LinuxIoPrio.*;
 
 public class LinuxPOSIXTest {
 
     @ClassRule
-    public static ConditionalTestRule rule = new ConditionalTestRule() {
-        public boolean isSatisfied() {
-            Platform platform = Platform.getNativePlatform();
-            Platform.OS os = platform.getOS();
-            Platform.CPU cpu = platform.getCPU();
-
-            if (os != Platform.OS.LINUX) return false;
-
-            switch (cpu) {
-                case PPC:
-                case PPC64:
-                case PPC64LE:
-                    return false;
-            }
-
-            return true;
-        }
-    };
+    public static RunningOnLinux rule = new RunningOnLinux();
 
     private static Linux linuxPOSIX = null;
 
@@ -206,3 +192,8 @@ public class LinuxPOSIXTest {
     }
 }
 
+class RunningOnLinux extends ConditionalTestRule {
+    public boolean isSatisfied() {
+        return jnr.ffi.Platform.getNativePlatform().getOS().equals(Platform.OS.LINUX);
+    }
+}
